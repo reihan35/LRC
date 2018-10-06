99889aac87521984fc0a7b52397f59bb7960fe0d
laudao <nguyen.laura97@gmail.com>
2018-10-06 12:43:53 +0200

    ex5

--- TP2/ex4.pl
+++ TP2/ex4.pl
@@ -6,8 +6,20 @@ parent(X,Y):- mere(X,Y); pere(X,Y).
 parent(X,Y,Z):- mere(Y,Z), pere(X,Z).
 grandPere(X,Y) :-  (pere(Z,Y), pere(X,Z));(mere(Z,Y), pere(X,Z)).
 frereOuSoeur(X,Y) :- pere(Z,X),pere(Z,Y);mere(Z,X),mere(Z,Y).
-ancetre(X,Y) :- pere(X,Y); grandPere(X,Y).
-/*
+
+/* V1
+ancetre(X,Y) :- parent(X,Y).
+ancetre(X,Y) :- parent(X,Z), ancetre(Z,Y). */
+
+/* V2
+ancetre(X,Y) :- parent(X,Z), ancetre(Z,Y).
+ancetre(X,Y) :- parent(X,Y). */
+
+/* V3 */
+ancetre(X,Y) :- parent(X,Y).
+ancetre(X,Y) :- ancetre(Z,Y),parent(X,Z).
+
+/* 2.
 ?- parent(homeira,fati).
 true [print]
 true .
@@ -20,12 +32,28 @@ Laura = homeira .
 
 ?- parent(laura,fati).
 false.
-
 */
 
-/*?- grandPere(parviz,fati).
+/* 4.
+?- parent(morteza,homeira,fati).
+true .
+
+?- parent(homeira,morteza,fati).
+false
+*/
+
+/* 5.
+?- grandPere(parviz,fati).
 true.
 */
 /*?- frereOuSoeur(laura,fati).
+true .
+*/
+
+/* 6. Mêmes résultats pour toutes les versions
+?- ancetre(parviz,fati).
+true .
+
+?- ancetre(homeira,fati).
 true .
 */
